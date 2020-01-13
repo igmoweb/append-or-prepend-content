8ce3378f6092a0a2a80912a0568dc2ed64bef836
Ignacio Cruz <igmoweb@gmail.com>
2020-01-13 17:44:36 +0100

    Fixed translations

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,6 +43,7 @@ module.exports = function( grunt ) {
 			options: {
 				type: 'wp-plugin',
 				domainPath: 'languages',
+        exclude: [ 'node_modules/*', 'vendor/*', 'tests/*', 'build/*', 'svn/*' ]
 			},
 			dist: {
 				options: {
