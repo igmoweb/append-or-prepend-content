e58af63dfe24655999118915596af8d1d5c64c86
Ignacio Cruz <igmoweb@gmail.com>
2022-03-23 22:01:47 +0100

    Add new submenu under Settings page

--- inc/admin.php
+++ inc/admin.php
@@ -5,13 +5,25 @@
 
 namespace AppOrPrepend\Admin;
 
-use const AppOrPrepend\PostType\POST_TYPE;
+use AppOrPrepend\PostType;
 
 /**
  * Inititialize hooks.
  */
 function init() {
 	add_action( 'admin_init', __NAMESPACE__ . '\\register_fields' );
+	add_action( 'admin_menu', __NAMESPACE__ . '\\add_settings_submenu' );
+}
+
+/**
+ * Add new submenu under Settings.
+ *
+ * @return void
+ */
+function add_settings_submenu() {
+	$post_type        = PostType\POST_TYPE;
+	$post_type_object = get_post_type_object( $post_type );
+	add_options_page( __( 'Append or Prepend Content', 'apporprepp' ), __( 'Append or Prepend Content', 'apporprepp' ), $post_type_object->cap->edit_posts, "edit.php?post_type=${post_type}" );
 }
 
 /**
@@ -30,8 +42,8 @@ function register_fields() {
  * Render the writing option.
  */
 function render_option() {
-	$view_link = admin_url( 'edit.php?post_type=' . POST_TYPE );
-	$new_link  = admin_url( 'post-new.php?post_type=' . POST_TYPE );
+	$view_link = admin_url( 'edit.php?post_type=' . PostType\POST_TYPE );
+	$new_link  = admin_url( 'post-new.php?post_type=' . PostType\POST_TYPE );
 	?>
 	<table class="form-table" role="presentation">
 		<tbody>
